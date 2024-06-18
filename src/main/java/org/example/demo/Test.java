c3801b0d80691a5c8253e597a6e9c67a43468f90
simon.xu <623533225@qq.com>
2024-06-18 09:09:33 +0800

    ok

--- src/main/java/org/example/demo/Test.java
+++ src/main/java/org/example/demo/Test.java
@@ -1,2 +1,7 @@
-package org.example.demo;public class Test {
+package org.example.demo;
+
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
 }
